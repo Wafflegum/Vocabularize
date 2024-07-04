10cc8a8515b54bf9fa19d97e7556c2d4f6530201
Wafflegum <70809819+Wafflegum@users.noreply.github.com>
2024-07-04 15:16:29 +0800

    Added pronunciation audio Changed font and polished the spacing

--- src/assets/components/ResultCard/ResultCard.jsx
+++ src/assets/components/ResultCard/ResultCard.jsx
@@ -1,35 +1,43 @@
 import React from 'react'
+import useSound from 'use-sound';
+
 import './ResultCard.css'
 
 const ResultCard = ({ wordData }) => {
-  return (
-    <card>
-        <div className="header">
-            <div className="title">
-                {wordData.word}
-            </div>
-            <div className="phonetics">
-                {wordData.phonetic}
-            </div>
-        </div>
+    const [playSound] = useSound(wordData.phonetics[wordData.phonetics.length - 1].audio)
+    console.log(wordData.phonetics)
 
-        <div className="meaning">
-            {wordData.meanings.map((def) => {
+    return (
+        <card>
+            <div className="header">
+                <div className="title">
+                    {wordData.word}
+                </div>
+                <div className="phonetics">
+                    {wordData.phonetic}
+                </div>
+                <div className="pronunciation" onClick={playSound}>
+                    <img src="https://img.icons8.com/?size=100&id=2795&format=png&color=000000" alt="play audio"  />
+                </div>
+            </div>
 
-                return (
-                    <div className="definitions" key={def.id}> 
-                        {def.partOfSpeech} 
-                        {def.definitions.slice(0, 3).map(e => {
-                            return (<span>{e.definition}</span>)
-                        })}
-                        {def.definitions.example}
-                    </div>
-                )
-            })}
-            
-        </div>
-    </card>
-  )
+            <div className="meaning">
+                {wordData.meanings.map((def) => {
+
+                    return (
+                        <div className="definitions" key={def.id}> 
+                            <div className="subtitle">{def.partOfSpeech.toUpperCase()}</div>
+                            {def.definitions.slice(0, 3).map(e => {
+                                return (<span key={e.index}>- {e.definition}</span>)
+                            })}
+                            {def.definitions.example}
+                        </div>
+                    )
+                })}
+                
+            </div>
+        </card>
+    )
 }
 
 export default ResultCard
