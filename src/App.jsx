3f3832062eb22d5d58ddfb546acbd4dcac37e9a9
Wafflegum <70809819+Wafflegum@users.noreply.github.com>
2024-12-22 06:14:01 +0800

    Improved mobile layout.
    - made the result card height to 100%, making it "fit" the screen

--- src/App.jsx
+++ src/App.jsx
@@ -6,65 +6,80 @@ import SearchBar from './assets/components/SearchBar/SearchBar'
 import ResultCard from './assets/components/ResultCard/ResultCard'
 
 function App() {
-  const [wordData, setWordData] = useState(null)
+	const [wordData, setWordData] = useState(null)
 
-  function SetDarkMode() {
-    document.querySelector('body').setAttribute('theme', 'dark');
-  }
+	function SetDarkMode() {
+		document.querySelector('body').setAttribute('theme', 'dark')
+	}
 
-  function SetLightMode() {
-    document.querySelector('body').setAttribute('theme', 'light');
-  }
+	function SetLightMode() {
+		document.querySelector('body').setAttribute('theme', 'light')
+	}
 
-  function ToggleTheme (element) {
-    if(element.target.checked)
-      SetDarkMode();
-    else
-      SetLightMode();
-  }
+	function ToggleTheme(element) {
+		if (element.target.checked) SetDarkMode()
+		else SetLightMode()
+	}
 
-  const checker = (data) => {
-    data ? data : ''
-  }
+	const checker = (data) => {
+		data ? data : ''
+	}
 
-  async function FetchWord(word) {
-    axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-      .then(response => 
-        {
-          setWordData(response.data[0])
-        })
-      .catch(error => {alert('Word not found' + ' ' + error.message)})
-  }
+	async function FetchWord(word) {
+		axios
+			.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+			.then((response) => {
+				setWordData(response.data[0])
+			})
+			.catch((error) => {
+				alert('Word not found' + ' ' + error.message)
+			})
+	}
 
-  return (
-    <main>
-      
-      <SearchBar fetch={FetchWord}/>
-      {
-          wordData ? <ResultCard 
-            word={wordData.word ?? ''}
-            phonetics={wordData.phonetic ?? ''}
-            meanings={wordData.meanings ?? ''}
-            audio={wordData.phonetics && wordData.phonetics.Length > 0 ? wordData.phonetics[wordData.phonetics.length - 1].audio : ''}/> 
-        : 
-          <ResultCard/>
-      }
+	return (
+		<main>
+			<SearchBar fetch={FetchWord} />
+			{wordData ? (
+				<ResultCard
+					word={wordData.word ?? ''}
+					phonetics={wordData.phonetic ?? ''}
+					meanings={wordData.meanings ?? ''}
+					audio={
+						wordData.phonetics && wordData.phonetics.Length > 0
+							? wordData.phonetics[wordData.phonetics.length - 1].audio
+							: ''
+					}
+				/>
+			) : (
+				<ResultCard />
+			)}
 
-      <div className="footer">
-        <label>
-          <input type="checkbox" id='themeToggle' onChange={ToggleTheme}/>
-          <span className="toggle"></span>
-          <img src="https://img.icons8.com/?size=100&id=101342&format=png&color=2D3A47" alt="Dark mode" className='dark' />
-          <img src="https://img.icons8.com/?size=100&id=60002&format=png&color=000000" alt="" className='light' />
-        </label>
+			<div className="footer">
+				<label>
+					<input type="checkbox" id="themeToggle" onChange={ToggleTheme} />
+					<span className="toggle"></span>
+					<img
+						src="https://img.icons8.com/?size=100&id=101342&format=png&color=2D3A47"
+						alt="Dark mode"
+						className="dark"
+					/>
+					<img
+						src="https://img.icons8.com/?size=100&id=60002&format=png&color=000000"
+						alt=""
+						className="light"
+					/>
+				</label>
 
-        <span>
-          Created by
-          <a href="https://github.com/Wafflegum" target='_blank'> Wafflegum </a>
-        </span>
-        </div>
-    </main>
-  )
+				<span>
+					Created by
+					<a href="https://github.com/Wafflegum" target="_blank">
+						{' '}
+						Wafflegum{' '}
+					</a>
+				</span>
+			</div>
+		</main>
+	)
 }
 
 export default App
--- src/assets/components/ResultCard/ResultCard.jsx
+++ src/assets/components/ResultCard/ResultCard.jsx
@@ -1,57 +1,50 @@
 import React from 'react'
-import useSound from 'use-sound';
+import useSound from 'use-sound'
 
 import './ResultCard.css'
 
 const ResultCard = ({ word, phonetics, meanings, audio }) => {
-    const [playSound] = useSound(audio)
+	const [playSound] = useSound(audio)
 
-    return (
-        <card>
-            {
-                word ? 
-                    ''
-                : 
-                    <>
-                        <h2 style={{textAlign: 'center'}}>Welcome to Vocabularize!</h2>
-                        <p style={{textAlign: 'center'}}>This is a simple dictionary created with react + dictionarydev.api.</p>
-                    </>
-            }
-            <div className="header">
-                <div className="title">
-                    {word}
-                </div>
-                <div className="phonetics">
-                    {phonetics ? phonetics : ''}
-                </div>
-                {
-                    audio ? 
-                        <div className="pronunciation" onClick={audio ? playSound : null}>
-                            <img src="https://img.icons8.com/?size=100&id=2795&format=png&color=000000" alt="play audio"  />
-                        </div>
-                     : ''
+	return (
+		<card>
+			{word ? (
+				''
+			) : (
+				<>
+					<h2 style={{ textAlign: 'center' }}>Welcome to Vocabularize!</h2>
+					<p style={{ textAlign: 'center' }}>
+						This is a simple dictionary created with react + dictionarydev.api.
+					</p>
+				</>
+			)}
+			<div className="header">
+				<div className="title">{word}</div>
+				<div className="phonetics">{phonetics ? phonetics : ''}</div>
+				{audio ? (
+					<div className="pronunciation" onClick={audio ? playSound : null}>
+						<img src="https://img.icons8.com/?size=100&id=2795&format=png&color=000000" alt="play audio" />
+					</div>
+				) : (
+					''
+				)}
+			</div>
 
-                }
-                
-            </div>
-
-            <div className="meaning">
-                {meanings?.map((def) => {
-
-                    return (
-                        <div className="definitions" key={def.id}> 
-                            <div className="subtitle">{def.partOfSpeech.toUpperCase()}</div>
-                            {def.definitions.slice(0, 3).map(e => {
-                                return (<span key={e.index}>- {e.definition}</span>)
-                            })}
-                            {def.definitions.example}
-                        </div>
-                    )
-                })}
-                
-            </div>
-        </card>
-    )
+			<div className="meaning">
+				{meanings?.map((def) => {
+					return (
+						<div className="definitions" key={def.id}>
+							<div className="subtitle">{def.partOfSpeech.toUpperCase()}</div>
+							{def.definitions.slice(0, 3).map((e) => {
+								return <span key={e.index}>- {e.definition}</span>
+							})}
+							{def.definitions.example}
+						</div>
+					)
+				})}
+			</div>
+		</card>
+	)
 }
 
 export default ResultCard
